8d6d3494a21d6ebcd841db5319bf93881941cf5c
tytnadpis <jenny0905007@gmail.com>
2026-05-04 02:13:15 +0300

    Test Render deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Финальная проверка!!! (пожалуйста)</h1>')
+  res.send('<h1>Облачное развёртывание</h1>')
 })
 
 const PORT = 8080
